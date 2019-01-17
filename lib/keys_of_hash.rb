436323069ec99b58afa7d406ec1c2a5be4a11f8a
Luis Zabala <luis.zabala076@gmail.com>
2019-01-17 22:49:41 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,4 +5,4 @@ class Hash
 end
 end
 
-#We have a method 'key_of' using a Splat to capture all the arguments being passed into the hash.
+#We have a method 'key_of' using a Splat to capture all the arguments being passed into the hash.  It will take the keys and values, question if the arguments include the values, and removed.
